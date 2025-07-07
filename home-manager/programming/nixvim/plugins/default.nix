3605038ab9428fb47a52666978f153a9480cf849
Librecht Kuijvenhoven <librecht@live.nl>
2025-07-07 23:07:40 +0200

    wrong filename

--- home-manager/programming/nixvim/plugins/default.nix
+++ home-manager/programming/nixvim/plugins/default.nix
@@ -6,7 +6,7 @@
     ./lsp.nix
     ./web-dev-icons.nix
     ./neo-tree.nix
-    ./treesitter.nix # Assuming you'll create this for syntax highlighting
+    ./tree-sitter.nix # Assuming you'll create this for syntax highlighting
     # Add any other plugin configuration files here
     # ./which-key.nix
     # ./gitsigns.nix
